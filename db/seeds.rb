83f2f0ce249d0c17cffee2f57a7a9c49e521e971
Nidhi Sarvaiya <sarvaiya.nidhi@gmail.com>
2023-01-04 22:04:04 -0500

    Generated Post, Search stimulus controller and seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+25.times do
+  Post.create(
+    title: Faker::Book.title,
+    body: Faker::Lorem.paragraph(sentence_count: 2)
+  )
+end
+
+Post.reindex
