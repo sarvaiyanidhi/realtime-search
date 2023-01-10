73c8140d719cb1e12d6a8380b5da9b32cff1cedd
Nidhi Sarvaiya <sarvaiya.nidhi@gmail.com>
2023-01-09 22:47:50 -0500

    Implemented automcomplete logic for search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -12,6 +12,20 @@ class SearchController < ApplicationController
     end
   end
 
+
+  def suggestions
+    @results = search_for_posts
+
+    respond_to do |format|
+      format.turbo_stream do
+        render turbo_stream:
+            turbo_stream.update('suggestions',
+                                partial: 'search/suggestions',
+                                locals: { results: @results })
+      end
+    end
+  end
+
   private
   def search_for_posts
     if params[:query].blank?
